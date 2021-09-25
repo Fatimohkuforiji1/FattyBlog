9c8c984493bc3f8a10921e442cc45ab3a4ad959d
Fatimohkuforiji1 <fatimohkuforiji@yahoo.com>
2021-09-25 17:55:48 +0100

    Used map to iterate through the data

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import { React, useState } from "react";
 
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new website",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 1,
+    },
+    {
+      title: "Welcome party!",
+      body: "lorem ipsum...",
+      author: "yoshi",
+      id: 2,
+    },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
-export default Home
+  return (
+    <div className="home">
+      {blogs.map((blog) => {
+        return (
+          <div className="blog-preview" key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Home;
+
+// const Home = () => {
+//   const [name, setName] = useState("Fatty");
+//   const [age, setAge] = useState(20);
+
+//   const handleClick = () => {
+//     setName("Yetty");
+//     setAge(30);
+//   };
+//   return (
+//     <div>
+//       <h4>HomePage</h4>
+//       <p>
+//         {name} is {age} years old
+//       </p>
+//       <button onClick={handleClick}>Click Me</button>
+//     </div>
+//   );
+// };
