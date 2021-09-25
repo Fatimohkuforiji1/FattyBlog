c40fee7329fa42be78357fee27fc865999449de9
Fatimohkuforiji1 <fatimohkuforiji@yahoo.com>
2021-09-25 17:56:41 +0100

    refactor some code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  return (
+  
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// function App() {
+//   const myblog = "Welcome to my Blog";
+//   return (
+//     <div className="App">
+//       <Navbar />
+//       <h3>{myblog}</h3>
+//       <div className="content">
+//         <Home />
+//       </div>
+//     </div>
+//   );
+// }
