b14fc767d975949a7b1ec00eb69b5d4bdf8fcfb3
Fatimohkuforiji1 <fatimohkuforiji@yahoo.com>
2021-09-30 12:23:59 +0100

    Rendered the body of the blog

---
+++ src/Components/BlogDetails.js
@@ -0,0 +1,29 @@
+import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
+
+const BlogDetails = () => {
+  const { id } = useParams();
+  const {
+    data: blog,
+    error,
+    isLoading,
+  } = useFetch("http://localhost:8000/blogs/" + id);
+
+  console.log(blog);
+
+  return (
+    <div className="blog-details">
+      {isLoading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div><p>{blog.body}</p></div>
+        </article>
+      )}
+    </div>
+  );
+};
+
+export default BlogDetails;
