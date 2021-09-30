247cf64d105892006b8ebfa7a95de94e685da11c
Fatimohkuforiji1 <fatimohkuforiji@yahoo.com>
2021-09-30 12:24:57 +0100

    Wrap the App component with Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
-
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import Create from "./Create";
+import BlogDetails from "./Components/BlogDetails";
 function App() {
-  
-    return (
-    <div className="App">
-      <Navbar/>
-
-      <div className="content">
-        <Home />
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
